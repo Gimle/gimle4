b7ef983bd2effd05061f80887858a09a34013ec8
Tux Solbakk <tux@krageroby.no>
2012-01-19 21:59:03 +0100

    Lots of changes to the dump function.
    Changed _fixDumpString function to a anonymous function.
    Changed _getCalleeFirstParam inline code.
    Fixed a bug where arrays was not dumped.
    Moved parent class name after to extends keyword from before class name.
    Added lookup for interfaces.
    Added lookup for class constants.
    Added lookup for class static variables.
    Removed the public append from public variables.
    Fixed private and protected appends to variables.
    Removed the display of the class namespace when dumping private variables.

--- lib/gimle/core/debug.php
+++ lib/gimle/core/debug.php
@@ -25,7 +25,23 @@ class Debug {
 //			return;
 //		}
 
-		$dodump = function ($var, $var_name = null, $indent = 0) use (&$dodump) {
+		$fixDumpString = function ($name, $value) {
+			if (in_array($name, array('[\'pass\']', '[\'password\']', '[\'PHP_AUTH_PW\']'))) {
+				$value = '********';
+			}
+			else {
+				$fix = array(
+					"\r\n" => self::color('¤¶', 'gray') . "\n", // Windows linefeed.
+					"\n\r" => self::color('¶¤', 'gray') . "\n\n", // Erronumous (might be interpeted as double) linefeed.
+					"\n"   => self::color('¶', 'gray') . "\n", // UNIX linefeed.
+					"\r"   => self::color('¤', 'gray') . "\n" // Old mac linefeed.
+				);
+				$value = strtr(htmlspecialchars($value), $fix);
+			}
+			return $value;
+		};
+
+		$dodump = function ($var, $var_name = null, $indent = 0) use (&$dodump, &$fixDumpString) {
 			if (strstr(print_r($var, true), '*RECURSION*') == true) {
 				echo call_user_func(__CLASS__ . '::color', 'Recursion detected, performing normal var_dump:', 'orange') . ' ';
 				echo $var_name . ' => ';
@@ -38,12 +54,12 @@ class Debug {
 			if (is_array($var)) {
 				echo ' => ' . call_user_func(__CLASS__ . '::color', 'Array (' . count($var) . ')', 'gray') . "\n" . str_repeat($doDump_indent, $indent) . "(\n";
 				foreach ($var as $key => $value) {
-					call_user_func(__CLASS__ . '::color', $value, '[\'' . $key . '\']', $indent + 1);
+					$dodump($value, '[\'' . $key . '\']', $indent + 1);
 				}
 				echo str_repeat($doDump_indent, $indent) . ')';
 			}
 			elseif (is_string($var)) {
-				echo ' = ' . call_user_func(__CLASS__ . '::color', 'String(' . strlen($var) . ')', 'gray') . ' ' . call_user_func(__CLASS__ . '::color', '\'' . call_user_func(__CLASS__ . '::_fixDumpString', $var_name, $var) . '\'', 'green');
+				echo ' = ' . call_user_func(__CLASS__ . '::color', 'String(' . strlen($var) . ')', 'gray') . ' ' . call_user_func(__CLASS__ . '::color', '\'' . $fixDumpString($var_name, $var) . '\'', 'green');
 			}
 			elseif (is_int($var)) {
 				echo ' = <span style="color: #a2a2a2;">Integer(' . strlen($var) . ')</span> ' . call_user_func(__CLASS__ . '::color', $var, 'red');
@@ -54,34 +70,53 @@ class Debug {
 			elseif (is_object($var)) {
 				$class = new \ReflectionObject($var);
 				$parents = '';
-				if ($class->getParentClass()) {
-					foreach ($class->getParentClass() as $value) {
-						$parents = $value . ' - ' . $parents;
-					}
+				if ($value = $class->getParentClass()) {
+					$parents .= ' extends ' . $value->name;
+				}
+				$interfaces = $class->getInterfaces();
+				if (!empty($interfaces)) {
+					$parents .= ' implements ' . implode(', ', array_keys($interfaces));
 				}
 
 				if ($var instanceof Iterator) {
-					echo ' => <span style="color: #a2a2a2;">' . $parents . get_class($var) . ' object (Iterator)' . "</span>\n" . str_repeat($doDump_indent, $indent) . "(\n";
+					echo ' => <span style="color: #a2a2a2;">' . get_class($var) . $parents . ' object (Iterator)' . "</span>\n" . str_repeat($doDump_indent, $indent) . "(\n";
 					var_dump($var);
 				}
 				else {
-					echo ' => <span style="color: #a2a2a2;">' . $parents . get_class($var) . ' object (' . count((array) $var) . ')' . "</span>\n" . str_repeat($doDump_indent, $indent) . "(\n";
+					echo ' => <span style="color: #a2a2a2;">' . get_class($var) . $parents . ' object (' . count((array) $var) . ')' . "</span>\n" . str_repeat($doDump_indent, $indent) . "(\n";
 					$reflect = new \ReflectionClass($var);
-					$props = $reflect->getProperties();
+					$constants = $reflect->getConstants();
+					if (!empty($constants)) {
+						foreach ($constants as $key => $value) {
+							$dodump($value, $key, $indent + 1);
+						}
+					}
+					$static = $reflect->getStaticProperties();
+					if (!empty($static)) {
+						foreach ($static as $key => $value) {
+							$visability = '';
+							if (!isset($var::$$key)) {
+								$visability = 'private|protected ';
+							}
+							$dodump($value, '[\'' . $key . '\': ' . $visability . 'static]', $indent + 1);
+						}
+					}
+					$namespace = $reflect->getNamespaceName();
 					foreach ((array) $var as $key => $value) {
 						if (!property_exists($var, $key)) {
 							if ((string) substr($key, 1, strlen(get_class($var))) == (string) get_class($var)) {
-								$keytype = 'protected';
+								$key = substr($key, (strlen((string) get_class($var)) + 1));
+								$keytype = ': private';
 							}
 							else {
 								$key = substr($key, 2);
-								$keytype = 'private';
+								$keytype = ': protected';
 							}
 						}
 						else {
-							$keytype = 'public';
+							$keytype = '';
 						}
-						$dodump($value, '[\'' . $key . '\':' . $keytype . ']', $indent + 1);
+						$dodump($value, '[\'' . $key . '\'' . $keytype . ']', $indent + 1);
 					}
 				}
 				echo str_repeat($doDump_indent, $indent) . ')';
@@ -110,11 +145,35 @@ class Debug {
 		echo "<!-- Dumping -->\n" . '<pre style="line-height: 120%; margin: 0px 0px 10px 0px; display: block; background: white; color: black; border: 1px solid #cccccc; padding: 5px; font-size: 10px;">';
 
 		if ($title === false) {
-			$dodump($var, self::_getCalleeFirstParam());
+			$backtrace = debug_backtrace();
+			if (substr($backtrace[0]['file'], -13) == 'eval()\'d code') {
+				$title = 'eval()';
+			}
+			else {
+				$con = explode("\n", file_get_contents($backtrace[0]['file']));
+				$callee = $con[$backtrace[0]['line'] - 1];
+				preg_match('/([a-zA-Z\\\\]+)::dump\((.*)/', $callee, $matches);
+				$i = 0;
+				$title = '';
+				foreach (str_split($matches[0], 1) as $value) {
+					if ($value === '(') {
+						$i++;
+					}
+					if (($i === 0) && ($value === ',')) {
+						break;
+					}
+					if ($value === ')') {
+						$i--;
+					}
+					if (($i === 0) && ($value === ')')) {
+						$title .= $value;
+						break;
+					}
+					$title .= $value;
+				}
+			}
 		}
-		else {
-			$dodump($var, $title);
-		}
+		$dodump($var, $title);
 		echo "</pre>\n<!-- Dump done -->\n";
 		if ($return == true) {
 			$out = ob_get_contents();
@@ -132,7 +191,7 @@ class Debug {
 	 * @param string $color
 	 * @return string
 	 */
-	public static function color ($content, $color) {
+	private static function color ($content, $color) {
 		$template = '<span style="color: %s;">%s</span>';
 		if ($color === 'gray') {
 			return sprintf($template, 'gray', $content);
@@ -152,62 +211,5 @@ class Debug {
 		else {
 			return $content;
 		}
-	}
-
-	/**
-	 * Remove passwords, and show hidden characters.
-	 *
-	 * @param string $name
-	 * @param mixed $value
-	 * @return string
-	 */
-	public static function _fixDumpString ($name, $value) {
-		if (in_array($name, array('[\'pass\']', '[\'password\']', '[\'PHP_AUTH_PW\']'))) {
-			$value = '********';
-		}
-		else {
-			$fix = array(
-				"\r\n" => self::color('¤¶', 'gray') . "\n", // Windows linefeed.
-				"\n\r" => self::color('¶¤', 'gray') . "\n\n", // Erronumous (might be interpeted as double) linefeed.
-				"\n"   => self::color('¶', 'gray') . "\n", // UNIX linefeed.
-				"\r"   => self::color('¤', 'gray') . "\n" // Old mac linefeed.
-			);
-			$value = strtr(htmlspecialchars($value), $fix);
-		}
-		return $value;
-	}
-
-	/**
-	 * Figure out how this class was called.
-	 *
-	 * @return string
-	 */
-	private static function _getCalleeFirstParam () {
-		$backtrace = debug_backtrace();
-		if (substr($backtrace[1]['file'], -13) == 'eval()\'d code') {
-			return 'eval()';
-		}
-		$con = explode("\n", file_get_contents($backtrace[1]['file']));
-		$callee = $con[$backtrace[1]['line'] - 1];
-		preg_match('/([a-zA-Z\\\\]+)::dump\((.*)/', $callee, $matches);
-		$i = 0;
-		$return = '';
-		foreach (str_split($matches[0], 1) as $value) {
-			if ($value === '(') {
-				$i++;
-			}
-			if (($i === 0) && ($value === ',')) {
-				break;
-			}
-			if ($value === ')') {
-				$i--;
-			}
-			if (($i === 0) && ($value === ')')) {
-				$return .= $value;
-				break;
-			}
-			$return .= $value;
-		}
-		return $return;
 	}
 }
