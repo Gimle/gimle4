6bdbf0c90812139fb1a9e22f5cc6428b6e6a9042
Tux Solbakk <tux@krageroby.no>
2012-01-29 15:28:48 +0100

    Added new mode for title in dump method.

--- lib/gimle/core/debug.php
+++ lib/gimle/core/debug.php
@@ -15,7 +15,7 @@ class Debug {
 	 *
 	 * @param mixed $var The variable to dump.
 	 * @param bool $return Return output? (Default: false)
-	 * @param string|bool Alternate title for the dump, or false to backtrace.
+	 * @param string|bool|array $title Alternate title for the dump, or to backtrace.
 	 * @return void|string
 	 */
 	public static function dump ($var, $return = false, $title = false, $mode = 'auto') {
@@ -182,32 +182,48 @@ class Debug {
 		}
 		echo '<pre style="line-height: 120%; margin: 0px 0px 10px 0px; display: block; border: 1px solid #cccccc; padding: 5px; font-size: 10px;' . ($background === 'black' ? ' background: black;' : '') . '">';
 
-		if ($title === false) {
+		if (($title === false) || (is_array($title))) {
 			$backtrace = debug_backtrace();
-			if (substr($backtrace[0]['file'], -13) == 'eval()\'d code') {
+			if ((is_array($title)) && (isset($title['steps'])) && (isset($backtrace[$title['steps']]))) {
+				$backtrace = $backtrace[$title['steps']];
+			}
+			else {
+				$backtrace = $backtrace[0];
+			}
+			if (substr($backtrace['file'], -13) == 'eval()\'d code') {
 				$title = 'eval()';
 			}
 			else {
-				$con = explode("\n", file_get_contents($backtrace[0]['file']));
-				$callee = $con[$backtrace[0]['line'] - 1];
-				preg_match('/([a-zA-Z\\\\]+)::dump\((.*)/', $callee, $matches);
-				$i = 0;
-				$title = '';
-				foreach (str_split($matches[0], 1) as $value) {
-					if ($value === '(') {
-						$i++;
-					}
-					if (($i === 0) && ($value === ',')) {
-						break;
-					}
-					if ($value === ')') {
-						$i--;
-					}
-					if (($i === 0) && ($value === ')')) {
+				$con = explode("\n", file_get_contents($backtrace['file']));
+				$callee = $con[$backtrace['line'] - 1];
+				if ((is_array($title)) && (isset($title['match']))) {
+					preg_match($title['match'], $callee, $matches);
+				}
+				else {
+					preg_match('/([a-zA-Z\\\\]+)::dump\((.*)/', $callee, $matches);
+				}
+				if (!empty($matches)) {
+					$i = 0;
+					$title = '';
+					foreach (str_split($matches[0], 1) as $value) {
+						if ($value === '(') {
+							$i++;
+						}
+						if (($i === 0) && ($value === ',')) {
+							break;
+						}
+						if ($value === ')') {
+							$i--;
+						}
+						if (($i === 0) && ($value === ')')) {
+							$title .= $value;
+							break;
+						}
 						$title .= $value;
-						break;
 					}
-					$title .= $value;
+				}
+				else {
+					$title = 'Unknown dump string';
 				}
 			}
 		}
